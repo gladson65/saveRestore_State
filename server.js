209d383d2776bcbae796b1e0ce139304cefa4e8b
gladson65 <grauth65@gmail.com>
2025-10-14 15:09:38 +0530

    info added

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 
+
+// Your app state is the set of variables that describe what’s currently happening in your app
+
 // create a server
 const app = new express();
 app.listen('7000', ()=> {
